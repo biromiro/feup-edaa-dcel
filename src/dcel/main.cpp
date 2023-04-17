edf034fce4993053748a9b8ab19e2fc0aac5455f
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-04-17 18:03:20 +0100

    Add json parsing library. Add sample use in main

--- src/dcel/main.cpp
+++ src/dcel/main.cpp
@@ -1,10 +1,19 @@
+// Copyright 2023 EDAA Group C
+#include <fstream>
 #include <iostream>
-#include "dcel.hpp"
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+#include "./dcel.hpp"
 
 int main() {
+  const auto* dcel = new DCEL<int>();
 
-    auto* vertex = new DCEL<int>();
+  std::ifstream f("example.json");
+  json data = json::parse(f);
 
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  std::cout << data["happy"] << std::endl;
+
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
 }
